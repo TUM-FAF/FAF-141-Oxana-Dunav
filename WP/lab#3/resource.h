1639a06ae9fbd287101863df689db6ea723d3e65
oxanadunav <oxana.dunav@gmail.com>
2016-03-24 14:24:49 +0200

    add lab#3

---
+++ WP/lab#3/resource.h
@@ -0,0 +1,19 @@
+#define IDB_TOOLS_GROUP       100
+#define IDB_PENCIL_TOOL       101
+#define IDB_LINE_TOOL         102
+#define IDB_BEZIER_TOOL       103
+#define IDB_RECTANGLE_TOOL    104
+#define IDB_ELLIPSE_TOOL      105
+#define IDB_ERASER_TOOL       106
+
+#define IDB_COLOR_GROUP       200
+#define IDB_FILL_CHECK        201
+
+#define IDB_WIDTH_GROUP       300
+#define IDB_BORDER_WIDTH      301
+
+#define IDB_CLEAR_BUTTON      400
+
+#define HK_DRAW_ELLIPSE       500
+#define HK_BLUE_COLOR         501
+
